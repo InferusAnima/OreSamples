2ff9357d1e7995130c5678e0d2a393afbde0fc7e
InferusAnima <InferusAnima@mail.ru>
2023-03-10 02:22:16 +0300

    Add items to generate script

--- scripts/block_generator.py
+++ scripts/block_generator.py
@@ -12,7 +12,7 @@ from pip._vendor.colorama import Fore
 def generate_states(states: list) -> None:
     os.makedirs("./blockstates/")
     for state in states:
-        state = f'{state.split(":")[1]}_sample'
+        state = f'{state[0].split(":")[1]}_sample'
         json_doc = {"variants": {"": [{"model": f"oresamples:block/{state}"}]}}
         with open(f"./blockstates/{state}.json", "w") as file:
             file.write(json.dumps(json_doc, indent=2))
@@ -26,6 +26,7 @@ def generate_states(states: list) -> None:
 def generate_block_models(states: list) -> None:
     os.makedirs("./models/block/")
     for state in states:
+        state = state[0]
         mod = state.split(":")[0]
         state = state.split(":")[1]
         json_doc = {
@@ -40,30 +41,61 @@ def generate_block_models(states: list) -> None:
 def generate_item_models(states: list) -> None:
     os.makedirs("./models/item/")
     for state in states:
-        state = f'{state.split(":")[1]}_sample'
+        state = f'{state[0].split(":")[1]}_sample'
         json_doc = {"parent": f"oresamples:block/{state}"}
         with open(f"./models/item/{state}.json", "w") as file:
             file.write(json.dumps(json_doc, indent=2))
             print(Fore.BLUE + f"Wrote ./models/item/{state}.json to disk" + Fore.RESET)
 
 
-def main(block_state: bool, item_model: bool, block_model: bool) -> None:
+def generate_loot_tables(states: list) -> None:
+    os.makedirs("./loot_tables/blocks/")
+    for state in states:
+        item = state[1]
+        state = f'{state[0].split(":")[1]}_sample'
+        json_doc = {
+            "type": "minecraft:block",
+            "pools": [
+                {
+                    "rolls": 1,
+                    "entries": [
+                        {
+                        "type": "minecraft:item",
+                        "name": item
+                        }
+                    ],
+                    "conditions": [
+                        {
+                        "condition": "minecraft:survives_explosion"
+                        }
+                    ]
+                }
+            ]
+        }
+
+        with open(f"./loot_tables/blocks/{state}.json", "w") as file:
+            file.write(json.dumps(json_doc, indent=2))
+            print(Fore.BLUE + f"Wrote ./loot_tables/blocks/{state}.json to disk" + Fore.RESET)
+
+
+def main(block_state: bool, item_model: bool, block_model: bool, loot_table: bool) -> None:
     """
     The main program logic which generates blockstates based on the array below
     Args:
         block_state (bool): trigger whether or not to generate blockstates
         item_model (bool): trigger whether or not to generate item models
         block_model (bool): trigger whether or not to generate block models
+        loot_table (bool): trigger whether or not to generate loot tables
     """
     states = [
-        "minecraft:coal_ore",
-        "minecraft:lapis_ore",
-        "minecraft:redstone_ore",
-        "minecraft:copper_ore",
-        "minecraft:iron_ore",
-        "minecraft:gold_ore",
-        "minecraft:diamond_ore",
-        "minecraft:emerald_ore",
+        ("minecraft:coal_ore", "minecraft:coal"),
+        ("minecraft:lapis_ore", "minecraft:lapis_lazuli"),
+        ("minecraft:redstone_ore", "minecraft:redstone"),
+        ("minecraft:copper_ore", "minecraft:raw_copper"),
+        ("minecraft:iron_ore", "minecraft:raw_iron"),
+        ("minecraft:gold_ore", "minecraft:raw_gold"),
+        ("minecraft:diamond_ore", "minecraft:diamond"),
+        ("minecraft:emerald_ore", "minecraft:emerald"),
     ]
 
     if block_state:
@@ -72,6 +104,8 @@ def main(block_state: bool, item_model: bool, block_model: bool) -> None:
         generate_item_models(states)
     if block_model:
         generate_block_models(states)
+    if loot_table:
+        generate_loot_tables(states)
 
 
 if __name__ == "__main__":
@@ -82,6 +116,7 @@ if __name__ == "__main__":
         print(Fore.CYAN + "    -i: Generate Item Models" + Fore.RESET)
         print(Fore.CYAN + "    -b: Generate Block Models" + Fore.RESET)
         print(Fore.CYAN + "    -s: Generate BlockStates" + Fore.RESET)
+        print(Fore.CYAN + "    -f: Generate LootTables" + Fore.RESET)
         print(Fore.CYAN + "    -a: Generate All" + Fore.RESET)
         print(Fore.CYAN + "    -h, ? : This screen" + Fore.RESET)
 
@@ -91,6 +126,7 @@ if __name__ == "__main__":
         "-i": False,
         "-b": False,
         "-s": False,
+        "-f": False,
         "-h": False,
         "?": False,
     }
@@ -109,6 +145,6 @@ if __name__ == "__main__":
         exit()
 
     if possible_args["-a"]:
-        main(True, True, True)
+        main(True, True, True, True)
     else:
-        main(possible_args["-s"], possible_args["-i"], possible_args["-b"])
+        main(possible_args["-s"], possible_args["-i"], possible_args["-b"], possible_args["-f"])

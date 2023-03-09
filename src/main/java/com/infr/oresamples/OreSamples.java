3cfcdd79860d39f0262c2c57ce3d65d5f1bea804
InferusAnima <InferusAnima@mail.ru>
2023-03-09 04:13:57 +0300

    Update OreSamples.java

--- src/main/java/com/infr/oresamples/OreSamples.java
+++ src/main/java/com/infr/oresamples/OreSamples.java
@@ -1,19 +1,29 @@
 package com.infr.oresamples;
 
+import com.mojang.logging.LogUtils;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
 
 @Mod("oresamples")
 public class OreSamples
 {
+    private static OreSamples instance;
+    public Logger LOGGER = LogUtils.getLogger();
     public OreSamples()
     {
+        instance = this;
+
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
 
         Registry.register(bus);
 
         MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    public static OreSamples getInstance() {
+        return instance;
     }
 }
